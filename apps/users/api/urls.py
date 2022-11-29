21d7e84cce2f68e8e4fa98b5ca7f716fb1ddaddd
David Enrique Palacios Garcia <paladavid@hotmail.com>
2022-11-29 13:37:36 -0500

    Get/Update/Delete User by ID done

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.users.api.api import UserApiView
+from apps.users.api.api import user_api_view, user_detail_id_api_view, user_detail_username_api_view
 
 urlpatterns = [
-    path('all/', UserApiView.as_view(), name='usuario_api')    
+    path('all/', user_api_view, name='usuario_api'),
+    path('all/<int:pk>/', user_detail_id_api_view, name='usuario_detail_api'), 
+    path('all/<str:username>/', user_detail_username_api_view, name='usuario_detail_api'),   
 ]

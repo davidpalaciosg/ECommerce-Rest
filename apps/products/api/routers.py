8c0f1681ad52b5ba6dcb0c13645125c92b6ece32
David Enrique Palacios Garcia <paladavid@hotmail.com>
2022-12-06 11:05:39 -0500

    ViewSets for all models created and working fine

--- apps/products/api/routers.py
+++ apps/products/api/routers.py
@@ -8,25 +8,17 @@ from apps.products.api.views import product_views as pv
 router = DefaultRouter()
 
 router.register(r'products', ProductViewSet, basename='products')
+router.register(r'measureunits', gv.MeasureUnitViewSet, basename='measure-units')
+router.register(r'categoryproducts', gv.CategoryProductViewSet, basename='category-products')
+router.register(r'indicators', gv.IndicatorViewSet, basename='indicators')
 
-#Include previous views
+#Include Aditional views
 urlpatterns = [
-    #Measure Unit CRUD
-    path('measureUnit/all', gv.MeasureUnitListApiView.as_view(), name='measure_unit_api'),
-    path('measureUnit/create', gv.MeasureUnitCreateApiView.as_view(), name='measure_unit_create_api'),
-    path('measureUnit/update/<int:pk>', gv.MeasureUnitUpdateApiView.as_view(), name='measure_unit_update_api'),
-    path('measureUnit/delete/<int:pk>', gv.MeasureUnitDestroyApiView.as_view(), name='measure_unit_delete_api'),
-    
-    #Category Product CRUD
-    path('categoryProduct/all', gv.CategoryProductListApiView.as_view(), name='category_product_api'),
-    path('categoryProduct/create', gv.CategoryProductCreateApiView.as_view(), name='category_product_create_api'),
-    path('categoryProduct/update/<int:pk>', gv.CategoryProductUpdateApiView.as_view(), name='category_product_update_api'),
-    path('categoryProduct/delete/<int:pk>', gv.CategoryProductDestroyApiView.as_view(), name='category_product_delete_api'),
-    
-    #Indicator CRUD
-    path('indicator/all', gv.IndicatorListApiView.as_view(), name='indicator_api'),
-    path('indicator/create', gv.IndicatorCreateApiView.as_view(), name='indicator_create_api'),
-    path('indicator/update/<int:pk>', gv.IndicatorUpdateApiView.as_view(), name='indicator_update_api'),
-    path('indicator/delete/<int:pk>', gv.IndicatorDestroyApiView.as_view(), name='indicator_delete_api'),
+    #path('measureUnit/all', gv.MeasureUnitListApiView.as_view(), name='measure_unit_api'),
+    #path('measureUnit/create', gv.MeasureUnitCreateApiView.as_view(), name='measure_unit_create_api'),
+    #path('measureUnit/update/<int:pk>', gv.MeasureUnitUpdateApiView.as_view(), name='measure_unit_update_api'),
+    #path('measureUnit/delete/<int:pk>', gv.MeasureUnitDestroyApiView.as_view(), name='measure_unit_delete_api'),
 ]
+
+#Include router urls
 urlpatterns += router.urls
--- apps/products/api/views/general_views.py
+++ apps/products/api/views/general_views.py
@@ -1,7 +1,14 @@
 from apps.shared.views.GenericViews import GeneralListApiView, GeneralCreateApiView, GeneralUpdateApiView, GeneralDestroyApiView
+from apps.shared.views.GenericViewSets import GeneralViewSet
 from apps.products.api.serializers.general_serializers import *
 
 #Measure Unit CRUD
+
+#Mesaure Unit ViewSet
+class MeasureUnitViewSet(GeneralViewSet):
+    serializer_class = MeasureUnitSerializer
+
+#Measure Unit API Views
 class MeasureUnitListApiView(GeneralListApiView):
     serializer_class = MeasureUnitSerializer
 class MeasureUnitCreateApiView(GeneralCreateApiView):
@@ -12,6 +19,12 @@ class MeasureUnitDestroyApiView(GeneralDestroyApiView):
     serializer_class = MeasureUnitSerializer
 
 #Category Product CRUD
+
+#Category Product ViewSet
+class CategoryProductViewSet(GeneralViewSet):
+    serializer_class = CategoryProductSerializer
+    
+#Category Product API Views
 class CategoryProductListApiView(GeneralListApiView):
     serializer_class = CategoryProductSerializer
 class CategoryProductCreateApiView(GeneralCreateApiView):
@@ -22,6 +35,12 @@ class CategoryProductDestroyApiView(GeneralDestroyApiView):
     serializer_class = CategoryProductSerializer
 
 #Indicator CRUD
+
+#Indicator ViewSet
+class IndicatorViewSet(GeneralViewSet):
+    serializer_class = IndicatorSerializer
+
+#Indicator API Views
 class IndicatorListApiView(GeneralListApiView):
     serializer_class = IndicatorSerializer
 class IndicatorCreateApiView(GeneralCreateApiView):

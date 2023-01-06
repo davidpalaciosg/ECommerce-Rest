d653c0220b4e959db6eb0ce60b09043f36987be1
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-01-06 13:34:47 -0500

    Search supplier by name view done

--- apps/expense_manager/api/views/general_views.py
+++ apps/expense_manager/api/views/general_views.py
@@ -1,9 +1,27 @@
 from apps.shared.views.GenericModelViewSets import GenericModelViewSet
 from apps.expense_manager.api.serializers.general_serializers import *
 
+#Import action decorator
+from rest_framework.decorators import action
+from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework import status
+
 #Supplier CRUD
 class SupplierViewSet(GenericModelViewSet):
     serializer_class = SupplierSerializer
+    
+    @action(detail=False, methods=['get'])
+    def search_supplier(self, request):
+        '''
+        Search a supplier by ruc or business name
+        '''
+        ruc_or_business_name = request.GET.get('ruc_or_business_name')
+        if ruc_or_business_name:
+            suppliers = Supplier.objects.filter(Q(ruc__icontains=ruc_or_business_name) | Q(business_name__icontains=ruc_or_business_name))
+            serializer = self.get_serializer(suppliers, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'error': 'Ruc or Business Name is required or not found'}, status=status.HTTP_400_BAD_REQUEST)
     
 #Payment Method CRUD
 class PaymentMethodViewSet(GenericModelViewSet):
